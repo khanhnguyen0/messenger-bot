abb4630e3446d6abbb57f924c5c1b0f31e654462
Khanh Nguyen <khanh@rnd.works>
2017-11-25 13:34:28 +0200

    fixed port bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/webhook')
 const process = require('process')
-const PORT = 80 || process.env.PORT
+const PORT = process.env.PORT  || 80 
 /* If running on debug mode ask Config manager for debug config. */
 const config = require('./config/config_manager')(process.env.production)
 require('http').createServer(app).listen(PORT, ()=>{
