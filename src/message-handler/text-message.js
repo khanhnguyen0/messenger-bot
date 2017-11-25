31e7444a6e1141c9f349a780dbed889b0722a0c5
Khanh Nguyen <khanh@rnd.works>
2017-11-25 13:19:05 +0200

    WIP

--- src/message-handler/text-message.js
+++ src/message-handler/text-message.js
@@ -1,4 +1,4 @@
-const apiai = require('apiai');
+const apiai = require('apiai-promise');
 
 const client = apiai('020e56ddc62c45d5898c84e717df0831')
 
@@ -8,33 +8,95 @@ const askEducation = require('../user-info/education')
 const askExperience = require('../user-info/experience')
 const askSkill = require('../user-info/skill')
 const User = require('../model/user.js');
-
+const state = {}
 
 mongoose.connect('mongodb://root:root@ds159254.mlab.com:59254/refugee')
 mongoose.Promise = require('bluebird')
 module.exports = async(senderId, message,quickReply) => {
-  const u = await User.findOne({id: senderId})
+
+    const u = await User.findOne({id: senderId})
     if (u)
     // return
     {
-      const skill = await askSkill(senderId,message)
-      if (skill) {
-        u.skill.push(skill)
+      console.log(state)
+      if (!state[senderId]) // no state, use wit to extract the user action
+      {
+        const {result} = await client.textRequest(message, {
+          sessionId: senderId
+        })
+        const {action} = result
+        console.log(action)
+        if (action!="input.unknown") state[senderId] = action
+        else
+        return send.textMessage(senderId,"Sorry I can't understand")
+        console.log(state)
+      }
+
+      // console.log(response)
+      switch (state[senderId]){
+        case 'create_skill':
+        const skill = await askSkill(senderId,message)
+        if (skill) {
+          state[senderId] = undefined // clear the bot state
+          u.skill.push(skill)
+          u.save((err,updated)=>{
+
+            if (err) return send.textMessage(senderId,"Error adding education")
+            return send.textMessage(senderId,"skill added")
+          })
+        }
+        break
+
+      case 'create_education':
+      const education = await askEducation(senderId,message)
+      if (education) {
+        state[senderId] = undefined // clear the bot state
+        u.education.push(education)
         u.save((err,updated)=>{
           if (err) return send.textMessage(senderId,"Error adding education")
-          return send.textMessage(senderId,"skill added")
+          return send.textMessage(senderId,"education added")
         })
       }
+      break
+
+      case 'create_experience':
+      const experience = await askExperience(senderId,message)
+      if (experience) {
+        state[senderId] = undefined // clear the bot state
+        u.experience.push(experience)
+        u.save((err,updated)=>{
+          if (err) return send.textMessage(senderId,"Error adding education")
+          return send.textMessage(senderId,"experience added")
+        })
+      }
+      break
+
+      case 'edit_education':
+      
+      break
+
+      case 'edit_skill':
+
+      break
+
+      case 'edit_experience':
+
+      break
+
+      default:
+      return send.textMessage(senderId,"Sorry I can't understand")
+      }
+
     } else {
       return createUser(senderId, message)
     }
 }
 
-const createUser = (senderId, message) => {
-  let request = client.textRequest(message, {
+const createUser = async (senderId, message) => {
+  const response = await client.textRequest(message, {
     sessionId: senderId
   })
-  request.on('response', function(response) {
+
     const {
       result
     } = response
@@ -74,7 +136,6 @@ const createUser = (senderId, message) => {
       return send.textMessage(senderId, fulfillment.speech)
     }
     console.log('RESPONSE: ', JSON.stringify(response, undefined, 2));
-  });
 
   request.on('error', function(error) {
     console.log(error);
